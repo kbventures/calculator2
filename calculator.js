3d9ccfb11a384f78bed9ec7b9c3cb34c415d8525
kbventures <kmb_x@hotmail.com>
2022-05-26 15:46:18 -0600

    Prototype V1

--- calculator.js
+++ calculator.js
@@ -1,13 +1,32 @@
+document.querySelectorAll('.button').forEach((node) => {
+    node.addEventListener('click', update);
+})
+
+const screen = document.querySelector('.screen');
+
+function update(event) {
+    let char = event.currentTarget.innerText;
+    let display = calc.calculate(char);
+    screen.innerText = display;
+
+}
+
+
+
+
+
 function Calculator() {
+
     this.state = '';
     this.operandsCount = 0;
 
-    this.operands = ['+', '-'];
+    this.operands = ['+', '-', '=', '/', '*'];
 
     this.calculate = (char) => {
-        this.stateCheck(char);
-        this.state += char;
 
+        if (this.stateCheck(char)) return this.state;
+        this.state += char;
+        return this.state;
     }
 
 
@@ -17,7 +36,14 @@ function Calculator() {
             this.operandsCount++;
         }
         if (this.operandsCount === 2) {
+            if (char === '=') {
+                this.state = Function("return " + this.state)();
+                this.operandsCount = 0;
+                return true;
+            }
             this.state = Function("return " + this.state)();
+            console.log(this.state)
+            this.operandsCount = 1;
         }
     }
 
@@ -25,11 +51,10 @@ function Calculator() {
 
 const calc = new Calculator();
 
-calc.calculate("1")
-calc.calculate("+")
-calc.calculate("1")
-calc.calculate("+")
+// calc.calculate()
+// calc.calculate("+")
+// calc.calculate("1")
+// calc.calculate("+")
 
-console.log(calc.state);
 
 
